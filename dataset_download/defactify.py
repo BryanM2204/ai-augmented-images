b99989b182c4efb7bc7f8862eb7c8190bf4a3add
Justin Latif <justin.latif@uconn.edu>
2026-04-14 18:16:00 -0400

    Set up the download for defactify and matched it with the celebdf one

---
+++ dataset_download/defactify.py
@@ -0,0 +1,37 @@
+import os
+from datasets import load_dataset
+from huggingface_hub import cached_download, HfFileSystem
+import shutil
+
+def download_and_setup_dataset():
+    print("Starting download for Defactify_Image_Dataset:")
+    
+    # Define the local data directory
+    data_dir = "./data/defactify"
+    
+    try:
+        # Create the directory if it doesn't exist
+        os.makedirs(data_dir, exist_ok=True)
+        print(f"\nUsing data directory: {data_dir}")
+        
+        # Download the dataset to the local directory (approx 7.5GB)
+        dataset = load_dataset("Rajarshi-Roy-research/Defactify_Image_Dataset",cache_dir=data_dir)
+
+        print("\nDownload Complete!")
+        print(f"\nStructure: {dataset}")        
+
+        return dataset
+    
+    except KeyboardInterrupt:
+        print("\nDownload interrupted by user.")
+        raise
+    
+    except Exception as e:
+        print(f"\nError downloading dataset: {str(e)}")
+        raise
+
+if __name__ == "__main__":
+    # Clear the cache before downloading
+    cache_dir = "~/.cache/huggingface/datasets/"
+    shutil.rmtree(cache_dir, ignore_errors=True)
+    ds = download_and_setup_dataset()
